05179d980a7fc34595d3b9c278f972e494fcfd0d
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-04-01 14:21:27 +0100

    Refactor

--- js/task-2.js
+++ js/task-2.js
@@ -13,7 +13,7 @@ class Storage {
     }
 
     removeItem(itemToRemove) {
-        const index = this.#items.findIndex((item) => item === itemToRemove);
+        const index = this.#items.indexOf(itemToRemove);
         if (index !== -1) {
             this.#items.splice(index, 1);
         }
